0c2d76d5440a3610f956b0b9287f68b94fed7048
Sidnei de Souza Junior <sidneisonic@hotmail.com>
2019-09-01 03:45:34 -0300

    Adicionando a nova pagina SobreNos

--- src/components/SobreNos.vue
+++ src/components/SobreNos.vue
@@ -1,9 +1,32 @@
-<template> 
-    <h1>Teste</h1>
+<template>
+  <v-content class="white col-md-12">
+    <h1 class="black-text mt-5" align="center">QUEM SOMOS</h1>
+    <v-row class="mt-10" align="center" justify="center">
+      <v-col cols="5">
+        <p
+          class="pr-10 text-justify headline font-weight-medium"
+        >Startup de soluções tecnológicas focada no desenvolvimento de sistemas personalizados. A Adsumus está habilitada a atender os mais v-diversos processos das empresas, automatizando tarefas rotineiras de forma integrada através de sistemas desktops, plataformas web, aplicativos e sites.</p>
+      </v-col>
+      
+      <v-col cols="5">
+        <v-img class="pl-10">
+          <v-row align="center" justify="center">
+            <v-img
+              src="../assets/somos.jpeg"                          
+            ></v-img>
+          </v-row>
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-content>
 </template>
 
 <script>
 export default {
-    
+  data() {
+    return {
+      altura: screen.height
+    }    
+  }
 }
 </script>
