37aab51c6e84a9e0724e727297e9e95fbbb8fc60
Sidnei de Souza Junior <sidneisonic@hotmail.com>
2019-09-02 00:06:27 -0300

    Adicionando CNPJ no Bottom do site

--- src/components/BottomAdsumus.vue
+++ src/components/BottomAdsumus.vue
@@ -2,7 +2,7 @@
   <v-bottom-navigation absolute class="font-weight-medium white--text black">
     <v-col class="text-center col-12 white--text">
       Copyright © {{ new Date().getFullYear() }}
-      <strong class="green--text ">Adsumus</strong> Desenvolvimento de Sistemas Ltda | Todos os direitos reservados.
+      <strong class="green--text ">Adsumus</strong> Desenvolvimento de Programas Ltda | CNPJ: 32.128.861/0001-46 | Todos os direitos reservados.
     </v-col>
   </v-bottom-navigation>
 </template>
